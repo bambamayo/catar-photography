49496d985d18213ce1c0d18502de978dc14928a1
bambamayo <ayobamiagunroye@gmail.com>
2020-01-24 08:25:23 +0100

    change manifest icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     author: `ayobami agunroye`,
   },
   plugins: [
-     {
+    {
       resolve: "gatsby-source-contentful",
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
@@ -51,7 +51,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
