fd7ede5e15d2cf8521769fb710cc5235cf361524
bambamayo <ayobamiagunroye@gmail.com>
2020-01-24 07:57:33 +0100

    change images modal icons

--- src/components/Gallery/ImageModal/ImageModal.js
+++ src/components/Gallery/ImageModal/ImageModal.js
@@ -14,10 +14,15 @@ const ImageModal = ({
       className="gallery-image-modal"
       style={showModal ? { width: "100%" } : { width: "0" }}
     >
-      <button className="gallery-image-modal-close" onClick={closeModal}>
+      <button
+        aria-label="close images modal icon"
+        className="gallery-image-modal-close"
+        onClick={closeModal}
+      >
         &#215;
       </button>
       <button
+        aria-label="go to previous image icon in images modal"
         style={index < 1 ? { display: "none" } : null}
         className="gallery-image-modal-left"
         onClick={arrowLeftClicked}
@@ -25,6 +30,7 @@ const ImageModal = ({
         &#8592;
       </button>
       <button
+        aria-label="go to next image icon in images modal"
         className="gallery-image-modal-right"
         style={index === arrLength ? { display: "none" } : null}
         onClick={arrowRightClicked}
